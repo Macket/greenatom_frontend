b7198e370e2de80f936cfd1f01be5721686e0ccd
Makeev Ivan <makeev@phystech.edu>
2019-11-05 11:57:23 +0300

    replaced webpack-dev-server with express

--- webpack.config.js
+++ webpack.config.js
@@ -45,12 +45,5 @@ module.exports = {
        extensions: ['.js', '.jsx'],
     },
 
-    devServer: {
-      port: 8080,
-      historyApiFallback: {
-          index: 'index.html'
-      }
-   },
-
     devtool: 'cheap-inline-module-source-map',
 };
